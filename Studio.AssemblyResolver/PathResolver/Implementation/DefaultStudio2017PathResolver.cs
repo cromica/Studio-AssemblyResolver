77ba1c1158fd39f94f7830872fab54ebe26794fa
andreaGhisa <aghisa@sdl.com>
2017-01-04 09:56:40 +0200

    Added support for Studio 2017.

---
+++ Studio.AssemblyResolver/PathResolver/Implementation/DefaultStudio2017PathResolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Studio.AssemblyResolver.PathResolver.Implementation
+{
+    internal class DefaultStudio2017PathResolver: IPathResolver
+    {
+        public string Resolve()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"SDL\SDL Trados Studio\Studio5\");
+        }
+    }
+}
---
+++ Studio.AssemblyResolver/PathResolver/Implementation/RegistryStudio2017PathResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Studio.AssemblyResolver.PathResolver.Implementation
+{
+    internal class RegistryStudio2017PathResolver: AbstractRegistryPathResolver
+    {
+        public override string GetStudioVersion()
+        {
+            return "Studio5";
+        }
+    }
+}
